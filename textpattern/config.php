7f463568ca7123dc8e4f6b134642db77039a7275
Joshua Vaage <joshuavaage@gmail.com>
2013-07-18 10:36:57 -0700

    initail commit

---
+++ textpattern/config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = 'dglarchitects';
+$txpcfg['user'] = 'root';
+$txpcfg['pass'] = 'root';
+$txpcfg['host'] = 'localhost';
+$txpcfg['table_prefix'] = '';
+$txpcfg['txpath'] = '/Applications/MAMP/htdocs/dglarchitects.dev/textpattern';
+$txpcfg['dbcharset'] = 'utf8';
+?>
